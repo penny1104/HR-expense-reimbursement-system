e4e237c37537c2094ee3d1a526e6308966aab9c6
程沛瑜 <123956461+penny1104@users.noreply.github.com>
2026-07-10 17:29:27 +0800

    Add files via upload

---
+++ hr-app/instance/config.py
@@ -0,0 +1,4 @@
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Alice20041104@localhost:3306/hr_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:your_password_here@localhost:5432/hr_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'dev-secret-key-change-in-production'
